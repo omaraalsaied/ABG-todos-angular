9cd29d16d6fb539d7e1297fb8bb3ac70df79592d
omaraalsaied <omar2181998@yahoo.com>
2025-02-10 04:44:34 +0200

    adding tailwind, creating header component, todo interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {AppHeaderComponent} from './components/app-header/app-header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [AppHeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'todos_app';
+  title = 'Todos';
 }
